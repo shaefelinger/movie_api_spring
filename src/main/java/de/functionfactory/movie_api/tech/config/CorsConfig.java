fbd6459d6eb3351f6ac886c4843cd739bd2b3f18
stha <stha@aprixon.de>
2024-10-28 06:45:54 +0100

    POST working

--- src/main/java/de/functionfactory/movie_api/tech/config/CorsConfig.java
+++ src/main/java/de/functionfactory/movie_api/tech/config/CorsConfig.java
@@ -5,19 +5,19 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-//@EnableWebMvc
-//public class CorsConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry corsRegistry) {
-//        corsRegistry
-//                .addMapping("/**")
-//                .allowedOrigins(
-//                        "myfrontend.com",
-//                        "http://127.0.0.1:63342"
-//                )
-//                .allowedMethods("GET","HEAD","PUT","PATCH","POST");
-////                .allowedMethods(HttpMethod.GET.name());
-//    }
-//}
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry
+                .addMapping("/**")
+                .allowedOrigins(
+                        "myfrontend.com",
+                        "http://127.0.0.1:63342"
+                )
+                .allowedMethods("GET","HEAD","PUT","PATCH","POST");
+//                .allowedMethods(HttpMethod.GET.name());
+    }
+}
--- src/main/java/de/functionfactory/movie_api/tech/config/ServerConfig.java
+++ src/main/java/de/functionfactory/movie_api/tech/config/ServerConfig.java
@@ -8,7 +8,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//
+
 //@Configuration
 //public class ServerConfig {
 //
